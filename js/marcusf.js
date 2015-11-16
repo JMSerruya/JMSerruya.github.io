53d7029d7430a9f46f99b63801176b6b20ef23f1
Marcus Frödin <marcusf@spotify.com>
2015-11-16 20:54:18 +0100

    More stuff.
    Change-Id: I76b269acc6ecdf881bf97a18f7b855b3865f4c5e

--- js/marcusf.js
+++ js/marcusf.js
@@ -2,20 +2,27 @@ function printPosts(blob) {
 	var output = "<ul>\n";	
 	var prefix = '<p class="medium-feed-snippet">';
 
-	for (var i = 0; i < blob.feed.entry.length; i++) {
-		var text = blob.feed.entry[i]["gs$cell"]["$t"];
-		if (i % 3 == 1) {
+	for (var i = 0; i < blob.length; i++) {
+		var text = blob[i].title, url = blob[i].link, descr = blob[i].summary;
+		
+			output += "\t" + '<li><a href="' + url + '">';
+
 			output += text + '</a> &middot; ';
-		} else if (i % 3 == 0) {
-			output += "\t" + '<li><a href="' + text + '">';
-		} else {
-			var starti = text.indexOf(prefix)
-			var endi = text.indexOf('</p>');
-			var snippet = text.substr(starti + prefix.length, endi - (starti+prefix.length));
-			output += snippet + "</li>\n";
-		}
+		
+			var starti = descr.indexOf(prefix)
+			var endi = descr.indexOf('</p>');
+			descr = descr.substr(starti + prefix.length, endi - (starti+prefix.length));
+			output += descr + "</li>\n";
 	}
 	output += "</ul>";
 	var node = document.getElementById('medium-link');
 	node.parentNode.innerHTML = output;
 }
+
+$(document).ready(function() {
+blockspring.runParsed("parse-rss-feed-to-json", { "feed_url": "https://medium.com/feed/@marcusf", "num_items": 0, "_cache": 7200}, { "api_key": "br_12599_5491dba6dc784f261e8ac7b5d80b3fcc031a2966" }, function(res){
+	console.log(res);
+    printPosts(res.params.feed);
+});
+});
+
